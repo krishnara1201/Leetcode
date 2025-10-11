ca412d951d9cd8049cf615ddbf2a5ecbbaaad246
Krish Lakshmi Narayanan <krish.narayanan1201@gmail.com>
2025-10-11 13:02:37 -0400

    Time: 1 ms (81.79%), Space: 18.1 MB (14.59%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,40 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        c1 = l1
-        t1 = 1
-        s1 = 0
-        while c1 != None:
-            s1 = s1 + t1 * (c1.val)
-            t1 = t1*10
-            c1 = c1.next
-        
-        c2 = l2
-        t2 = 1
-        s2 = 0
-        while c2 != None:
-            s2 = s2 + t2 * (c2.val) 
-            t2 = t2*10
-            c2 = c2.next
-        
-        sum_num = s1 + s2
-        new_head = ListNode(val = sum_num % 10, next = None)
-        sum_num = (sum_num - (sum_num % 10))/10
-        current = new_head
-        while sum_num != 0:
-            new_val = sum_num % 10
-            sum_num = (sum_num - (new_val))/10
-            new_node = ListNode(val = new_val, next = None)
-            current.next = new_node
-            current = current.next
-        
-        return new_head
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        cur1, cur2 = l1, l2
+        carry = 0
+        dummy = cur = ListNode(0, None)
+
+        while (cur1 or cur2) or (carry > 0):
+            v1 = cur1.val if cur1 else 0
+            v2 = cur2.val if cur2 else 0
+            
+            add = v1 + v2 + carry
+            carry = add // 10
+            
+            new = ListNode(add % 10, None)
+            cur.next = new
+            cur = new
+            if cur1:
+                cur1 = cur1.next
+            if cur2:
+                cur2 = cur2.next
+            
+        return dummy.next
