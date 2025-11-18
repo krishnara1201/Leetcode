7f67512c56067fa67f6d56e745cab6845e43731e
Krish Lakshmi Narayanan <krish.narayanan1201@gmail.com>
2025-11-17 20:08:47 -0500

    Time: 311 ms (54.58%), Space: 17.9 MB (59.38%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,53 +1,24 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        arr = s
-        l = len(arr)
-        i = 0
-        j = 0
-        sub = []
-        while i < l:
-            # case 1
-            sub_p1 = arr[i]
-            left = i
-            right = i
-            while (left > 0 and right < l - 1):
-                left = left - 1
-                right = right + 1
-                if arr[left] == arr[right]:
-                    sub_p1 = arr[left] + sub_p1 + arr[right]
-                else:
-                    break
-                
-            sub.append(sub_p1)
-            i += 1
-        
-        while j < l - 1:
-            # case 2
-            if arr[j] == arr[j+1]:
-                sub_p2 = arr[j] + arr[j+1]
-                left = j
-                right = j + 1
-                while (left > 0 and right < l - 1):
-                    left = left - 1
-                    right = right + 1
-                    if arr[left] == arr[right]:
-                        sub_p2 = arr[left] + sub_p2 + arr[right]
-                    else:
-                        break
-                sub.append(sub_p2)
-                j += 1   
-            else:
-                j += 1   
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res_index, maxlen = 0, 0
+        n = len(s)
+        for c in range(n):
+            l,r = c, c 
+            while (0 <= l < n and 0 <= r < n and
+                    s[l] == s[r]):
+                if (r - l + 1) > maxlen:
+                    res_index = l
+                    maxlen = r - l + 1
+                l -= 1
+                r += 1
 
-        maxi = -1
-        
-        for i in sub:
-            if len(i) > maxi:
-                res = i
-                maxi = len(i)
-        return res
-        
+            l,r = c-1, c
+            while (0 <= l < n and 0 <= r < n and
+            s[l] == s[r]):
+                if (r - l + 1) > maxlen:
+                    res_index = l
+                    maxlen = r - l + 1
+                l -= 1
+                r += 1
+
+        return s[res_index:res_index + maxlen]
