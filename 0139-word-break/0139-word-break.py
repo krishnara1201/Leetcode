f57965cfbf758d65ad47d2e78648504adee22f05
Krish Lakshmi Narayanan <krish.narayanan1201@gmail.com>
2025-11-23 23:01:09 -0500

    Time: 0 ms (100%), Space: 17.6 MB (99.8%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(n-1, -1, -1):
             for word in wordDict:
                 m = len(word)
-                if s[i:i+m] == word and dp[i+m]:
+                if (i + m <= n) and s[i:i+m] == word and dp[i+m]:
                     dp[i] = True
                     break
         
