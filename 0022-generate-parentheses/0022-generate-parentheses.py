a69d5d7fd7c215cc880c3a026789560358aa1dc9
Krish Lakshmi Narayanan <krish.narayanan1201@gmail.com>
2026-03-28 13:10:57 -0400

    Time: 0 ms (100%), Space: 19.5 MB (25.19%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,23 +1,19 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-
         stack = []
 
         def dfs(openB, closeB):
-            if openB == n and openB == closeB:
+            if openB == closeB == n:
                 res.append("".join(stack))
                 return
-            
             if openB < n:
                 stack.append("(")
                 dfs(openB + 1, closeB)
                 stack.pop()
-            
             if closeB < openB:
                 stack.append(")")
                 dfs(openB, closeB + 1)
                 stack.pop()
-            
         dfs(0,0)
         return res
