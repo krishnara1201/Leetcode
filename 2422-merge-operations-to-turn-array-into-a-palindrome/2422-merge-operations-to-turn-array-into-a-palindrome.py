228b3e785e70d4215071d0dfc434b9acb34a8c05
Krish Lakshmi Narayanan <krish.narayanan1201@gmail.com>
2026-05-22 18:45:33 -0400

    Time: 87 ms (35.48%), Space: 34.6 MB (51.61%) - LeetHub

---
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        l,r = 0, len(nums) - 1
+        swaps = 0
+
+        while l <= r:
+            if nums[l] == nums[r]:
+                l += 1
+                r -= 1
+            while nums[l] != nums[r] and l <= r:
+                if nums[l] < nums[r]:
+                    nums[l+1] += nums[l]
+                    l += 1
+                else:
+                    nums[r-1] += nums[r]
+                    r -= 1
+
+                swaps += 1
+        return swaps
