36cdea4f87264cb073a209719dfa73298ed11d49
Krish Lakshmi Narayanan <krish.narayanan1201@gmail.com>
2026-05-26 22:02:39 -0400

    Time: 16 ms (74.74%), Space: 19.3 MB (69.48%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -1,11 +1,10 @@
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-        dp = [0] * (n+1)
+        res = 0
         for i in range(1,n+1):
             if self.isrotatedvalid(i):
-                dp[i] += 1
-            dp[i] += dp[i-1]
-        return dp[n]
+                res += 1
+        return res
 
 
 
